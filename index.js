cf3b6b672ba61fc6fa8b60d7890aa2067df6c1c3
Sakib Rahman <seu.sakib@gmail.com>
2022-05-23 13:00:01 +0600

    set mongodb connection

--- index.js
+++ index.js
@@ -8,6 +8,21 @@ const app = express();
 
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_USER_PASS}@cluster0.0lwbw.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri);
+
+async function run() {
+  try {
+    await client.connect();
+    const toolCollection = client.db("alpha_tools").collection("tools");
+  } finally {
+    // await client.close();
+  }
+}
+
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
   res.send("Welcome to Alpha Tools Server");
 });
