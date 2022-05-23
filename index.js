d3179ac65ecf0b15e5f96ef47b69273ab1cfcce0
Sakib Rahman <seu.sakib@gmail.com>
2022-05-23 12:55:54 +0600

    add / api

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
+console.log(process.env);
 const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Alpha Tools Server");
+});
 
 app.listen(port, () => {
   console.log("Listening to port ->", port);
