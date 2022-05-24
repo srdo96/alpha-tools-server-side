669ed6b96acf48b3d972faf6c650119668f45761
Sakib Rahman <seu.sakib@gmail.com>
2022-05-25 00:38:34 +0600

    add delete API to delete order

--- index.js
+++ index.js
@@ -43,6 +43,12 @@ async function run() {
       const result = await orderCollection.find({ userEmail: email }).toArray();
       res.send(result);
     });
+    app.delete("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await orderCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
     // await client.close();
   }
